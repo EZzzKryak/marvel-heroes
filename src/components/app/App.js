9cf4bed59ce7b983bea84518b05a480978b13b0b
Alex <manicynaleksandr@yandex.ru>
2023-08-02 22:45:20 +0300

    comics routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,39 +1,22 @@
-import { useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
-import CharInfo from '../charInfo/CharInfo';
-import CharList from "../charList/CharList";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import RandomChar from "../randomChar/RandomChar";
-
-import decoration from "../../resources/img/vision.png";
+import { ComicsPage, MainPage, Page404, SingleComicPage } from "../pages";
 
 const App = () => {
-  const [selectedChar, setChar] = useState(null);
-
-  const [active, setActive] = useState(false);
-
-  const onCharSelected = id => {
-    setChar(id);
-  };
-
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        <ErrorBoundary>
-          <RandomChar />
-        </ErrorBoundary>
-        <div className="char__content">
-          <ErrorBoundary>
-            <CharList onCharSelected={onCharSelected} />
-          </ErrorBoundary>
-          <ErrorBoundary>
-            <CharInfo charId={selectedChar} />
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Routes>
+            <Route element={<MainPage />} path="/" />
+            <Route element={<ComicsPage />} path="comics" />
+            <Route element={<SingleComicPage />} path="comics/:comicId" />
+            <Route element={<Page404 />} path="*"></Route>
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/pages/index.js
@@ -0,0 +1,7 @@
+import Page404 from "./404";
+import ComicsPage from "./ComicsPage";
+import MainPage from "./MainPage";
+import SingleComicPage from "./singleComicPage";
+
+export { ComicsPage, MainPage, Page404, SingleComicPage };
+
